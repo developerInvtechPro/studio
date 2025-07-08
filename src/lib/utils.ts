b65db01595bb7884a92624561eace7d6783a00fc
Solmer Lopez <ieduardo111203@gmail.com>
2025-07-08 04:47:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { conversor } from 'numero-a-letras';
+import numeroALetras from 'numero-a-letras';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,7 +10,7 @@ export function cn(...inputs: ClassValue[]) {
 export function numberToWords(num: number): string {
   if (num === null || num === undefined) return '';
   try {
-    return conversor.aLetras(num, {
+    return numeroALetras.conversor.aLetras(num, {
       plural: 'LEMPIRAS',
       singular: 'LEMPIRA',
       centimos: {
